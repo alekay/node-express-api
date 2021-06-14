4ce08e5de52b0529005b0cc25d7c1814e51a0810
alekay <alekaymusic@gmail.com>
2021-06-14 18:22:13 -0400

    add edit and delete/destroy routes

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const app = express();
 const path = require('path');
 // UUID to generate IDs
 const { v4: uuid } = require('uuid');
+// overrides the method 
+const methodOverride = require('method-override');
 
 //To parse form data in POST request body:
 app.use(express.urlencoded({ extended: true }))
 // To parse incoming JSON in POST request body:
 app.use(express.json())
 
+app.use(methodOverride('_method'))
 // add view engine/EJS to the application so that we can input data in a form and not just request via Postman
 app.set('view engine', 'ejs')
 // this helps us make sure we are doing an absolute path
@@ -42,12 +45,12 @@ let comments = [
 
 // get comments path, refer to index.ejs file via index argument, render comments with {comments}
 app.get('/comments', (req, res) => {
-    res.render('comments/index', { comments });
+    res.render('comments/index', { comments })
 })
 
 // serves/renders a form/template for the comments
 app.get('/comments/new', (req, res) => {
-  res.render('comments/new');
+  res.render('comments/new')
 })
 
 // this is where the form submits its data to, pushing into the array (we will add a database later) - create a new comment
@@ -56,7 +59,7 @@ app.post('/comments', (req, res) => {
   const { username, comment } = req.body;
   comments.push({ username, comment, id: uuid() })
   // redirect back to the index.ejs to see the comments on the page
-  res.redirect('/comments');
+  res.redirect('/comments')
 })
 
 // get comment by id - show route - details around one particular comment by ID
@@ -64,10 +67,35 @@ app.get('/comments/:id', (req, res) => {
   // take the ID from the URL
   const { id } = req.params;
   // use array method FIND to find the correct comment in the comments array
-  // add parseInt to parse the string returned (the user ID will be returned as a string) int a number
   const comment = comments.find(c => c.id === id);
   // render the show comments page
-  res.render('comments/show', { comment });
+  res.render('comments/show', { comment })
+})
+
+// route to serve up a form for EDIT path
+app.get('/comments/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === id);
+  res.render('comments/edit', { comment })
+})
+
+// Update route - use PATCH
+app.patch('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  // find comment by id
+  const foundComment = comments.find(c => c.id === id);
+  // taking whatever is sent in the request body (payload)
+  const newCommentText = req.body.comment;
+  // update that comment, its comment property, to whatever what was in that request comment body
+  foundComment.comment = newCommentText;
+  res.redirect('/comments')
+})
+
+app.delete('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  // update the comments variable to be this new array based upon the old version of comments
+  comments = comments.filter(c => c.id !== id);
+  res.redirect('/comments')
 })
 
 // serve up the app on port 3000 - in terminal (in app directory) run -> nodemon app.js
