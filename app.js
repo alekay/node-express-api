fe48c6d0d79f3eb7dd432bc7d92d970fdb7ec46f
alekay <alekaymusic@gmail.com>
2021-06-13 21:29:23 -0400

    add comment post route and linkage between Index and New pages

--- app.js
+++ app.js
@@ -39,17 +39,31 @@ app.get('/comments', (req, res) => {
     res.render('comments/index', {comments})
 })
 
-app.get('/tacos', (req, res) => {
-  res.send("GET /tacos response")
+// serves/renders a form/template for the comments
+app.get('/comments/new', (req, res) => {
+  res.render('comments/new')
 })
 
-// post route
-app.post('/tacos', (req, res) => {
-  // This establishes the request objects we are going to send in the request body (req.body)
-  const {meat, qty} = req.body;
-  // this is the response that will be returned on the page body based on our input on the FORM
-  res.send(`OK here are your ${qty} ${meat} tacos`)
+// this is where the form submits its data to, pushing into the array (we will add a database later)
+app.post('/comments', (req, res) => {
+  // const username, comment are the fields we are going to push with req.body to the index.ejs body that holds the array
+  const {username, comment} = req.body;
+  comments.push({username, comment})
+  // redirect back to the index.ejs to see the comments on the page
+  res.redirect('/comments')
 })
+
+// app.get('/tacos', (req, res) => {
+//   res.send("GET /tacos response")
+// })
+
+// // post route
+// app.post('/tacos', (req, res) => {
+//   // This establishes the request objects we are going to send in the request body (req.body)
+//   const {meat, qty} = req.body;
+//   // this is the response that will be returned on the page body based on our input on the FORM
+//   res.send(`OK here are your ${qty} ${meat} tacos`)
+// })
 
 // serve up the app on port 3000 - in terminal (in app directory) run -> nodemon app.js
 app.listen(3000, () => {
